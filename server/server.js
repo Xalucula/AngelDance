183dd029064c278700ad2a8157056dcd00c98b49
Mihail <misha17_74@mail.ru>
2023-05-10 01:18:16 +0300

    initial commit

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+// const authRouter = require('./routes/authRouter');
+
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors({ credentials: true, origin: true }));
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use('/api/auth', authRouter);
+
+app.listen(PORT, () => console.log(`Server has started on PORT ${PORT}`));
